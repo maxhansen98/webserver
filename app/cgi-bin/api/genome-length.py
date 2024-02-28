39269afa94ffb0ca615fccc7b342302acb736128
Jan Philipp Hummel <hummelj@jaspis.cip.ifi.lmu.de>
2024-02-28 12:07:43 +0100

    big bugfix

--- app/cgi-bin/api/genome-length.py
+++ app/cgi-bin/api/genome-length.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
 import json
-def execute_subprocess():
-    return {
-        'success': True,
-        'output': '123TEST'
-    }
- 
+import sys
+import subprocess
+import re
 
+
+def execute_subprocess(organism_patterns):    
+    command = ["python3", "/home/h/hummelj/propra/8_genome_report/genome_length.py", "--organism"]
+    organisms = re.findall(r'"([^"]*)"', organism_patterns)
+    for organism in organisms:
+        command.append(organism)
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    if result.returncode == 0:
+        return {
+            'success': True,
+            'output': result.stdout
+        }
+    else:
+        return {
+            'success': False,
+            'error': result.stderr
+        }
 def main():
-    # Set content type to JSON
     print("Content-Type: application/json\n")
+    body = json.loads(sys.stdin.read())
+    try:
+        organism_patterns = body.get('organism')
+        result = {'data':execute_subprocess(organism_patterns)}
+    except KeyError:
+        result = {'error': 'Missing organism_patterns'}
+    
 
-    # Execute subprocess
-    result = execute_subprocess()
+    
+
 
     # Print JSON response
     print(json.dumps(result))
--- app/js/tasks.js
+++ app/js/tasks.js
@@ -1,7 +1,7 @@
 
+//{ id:1, name: "Genome Report", repo_link: "https://gitlab2.cip.ifi.lmu.de/bio/propra_ws23/hummelj/blockgruppe3/-/tree/8_genome_report?ref_type=heads", input:{parameters:[{name:"Organism", required:true, type: "file", accept: "image/*, .pdf, .docx" , default:{path:"/home/h/hummelj/propra/blockgruppe3/var/default/genome"}}]}}
 var tasks = [
-    { id:1, name: "Genome Report", repo_link: "https://gitlab2.cip.ifi.lmu.de/bio/propra_ws23/hummelj/blockgruppe3/-/tree/8_genome_report?ref_type=heads", input:{parameters:[{name:"Organism", required:true, type: "file", accept: "image/*, .pdf, .docx" }]}}
-
+    { id:1, name: "Genome Report", repo_link: "https://gitlab2.cip.ifi.lmu.de/bio/propra_ws23/hummelj/blockgruppe3/-/tree/8_genome_report?ref_type=heads", input:{parameters:[{name:"Organism", required:true, type: "text", default:{path:"Escherichia coli; Actinomyces oris"}}]}},
 ];
 
 function openTasks(open_tasks) {
@@ -34,8 +34,9 @@ function openTasks(open_tasks) {
                         tasksHtml += `
                         <div class="flex flex-row items-center justify-start px-4 gap-2">
                             <p class="text-xs font-normal txt-lgt">${param.name}:</p>
-                            <form>
+                            <form id="formElem">
                                 <input class="box py-0.5 text-xs font-normal txt-lgt w-64" type="file" name="${param.name}" ${param.required ? 'required' : ''} ${param.accept ? 'accept="' + param.accept + '"' : ''}>
+                                <input type="submit">
                             </form>
                         </div>
                         `;
@@ -45,48 +46,50 @@ function openTasks(open_tasks) {
                   }
                 
             });
+            
+            const codeFontStyle = "font-family: 'Courier New', Courier, monospace; font-style: italic;";
+            
             tasksHtml += `
                         <div class="w-full flex flex-row items-center justify-end px-4 gap-4">
-                            <button><p class="lnk text-xs font-semibold">Remove data</p></a></button>
+                            <button><p class="lnk text-xs font-semibold">Undo</p></a></button>
                             <button onclick="
                             {
+                                
                                 const task = tasks.find(task => task.id === ${task.id});
-                                const formData = new FormData();
                                 const jsonData = {};
                                 task.input.parameters.forEach(function(param) {
                                     document.querySelectorAll(\'input\').forEach(function(p) {
                                         if (param.name===p.name){
                                             const input_id = p.name;
-                                            const input_data = p.files[0];
-                                            if (input_data !== undefined){                                               
-                                                jsonData[input_id.toLowerCase()]=input_data;                                               
+                                            const input_data = p.value;
+                                            if (input_data !== undefined){  
+   
+                                                jsonData[input_id.toLowerCase()]=input_data;                                             
                                             }
                                             
                                         }
                                         
                                     });
                                 });
+                                console.log('js: ', jsonData);
                                 fetch('http://bioclient1.bio.ifi.lmu.de/~hummelj/cgi-bin/api/genome-length.py', {
                                     method: 'POST',
-                                    headers: {
-                                        'Content-Type': 'application/json'
-                                    },
-                                    body: JSON.stringify(jsonData)
+                                    body: JSON.stringify(jsonData),
+                                    contentType: 'application/json'
                                 })
-                                .then(response => {
-                                    if (response.ok) {
-                                        console.log(response);  
-                                        
-                                    } else {
-                                        throw new Error('Something went wrong');
+                                .then(response => response.json())
+                                .then(data => {
+                                    console.log(data);
+                                    const outputSection = document.getElementById('outputSection');
+                                    outputSection.innerHTML = '';
+                                    for (const [key, value] of Object.entries(data)) {
+                                        const p = document.createElement('p');
+                                        p.className = 'text-xs font-normal txt-lgt';
+                                        p.innerHTML = key + ': ' + value.output;
+                                        outputSection.appendChild(p);
                                     }
-                                    
-                                })
-                                
-                                .catch(error => {
-                                    // Handle network errors or other exceptions
-                                    console.error('An error occurred', error);
-                                });
+                                }
+                                )
                                 
                             }
                             "><p class="lnk text-xs font-semibold">Run</p></a></button>
@@ -96,7 +99,7 @@ function openTasks(open_tasks) {
                     </div>
                     <div class="mt-2 border border-gray-300 rounded-lg px-4 py-2 flex flex-col justify-start items-start w-full gap-2">
                         <h1 class="text-sm font-semibold italic txt-lgt">Output:</h1>
-                        <div id="outputSection" class="w-full flex flex-col justify-start items-start gap-2"></div>
+                        <div id="outputSection" class="w-full flex flex-col justify-start items-start gap-2 px-4" style="${codeFontStyle}"></div>
                     </div>
                 </div>
             </div>
